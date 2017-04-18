3a7bed9a953d3eb01cd772245ee9770c9ff0a868
soler <solerwell@msn.com>
2017-04-18 20:16:45 +0800

    proxy pattern

---
+++ src/main/java/me/soler/designPattern/proxy/GamePlayerProxy.java
@@ -0,0 +1,28 @@
+package me.soler.designPattern.proxy;/**
+ * Created by soler on 2017/4/18.
+ */
+
+/**
+ * <p>游戏玩家代理类 </p>
+ *
+ * @author weizengjie
+ * @since 2017/4/18
+ */
+public class GamePlayerProxy implements IGamePlayer, Countable{
+
+    public void count() {
+
+    }
+
+    public void login(String name, String pwd) {
+
+    }
+
+    public void killBoss() {
+
+    }
+
+    public void upgrade() {
+
+    }
+}
